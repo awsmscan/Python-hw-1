50f7006481fa08bcfa03761cdec83a29030aeb47
awsmscan <maoqinyu226@gmail.com>
2020-11-27 02:39:12 -0500

    verison1.1

--- BondPrice_File.py
+++ BondPrice_File.py
@@ -1,5 +1,5 @@
 
 
 def getBondPrice(y, face, couponRate, m, ppy=1):
-    bond=face*couponRate/yc*(1-(1+y/yc)**-(m*yc))/(y/yc)+face/(1+y/yc)**(m*yc)
+    bond=face*couponRate/ppy*(1-(1+y/ppy)**-(m*ppy))/(y/ppy)+face/(1+y/ppy)**(m*ppy)
     return(bond)
--- test_UnitTests.py
+++ test_UnitTests.py
@@ -1,15 +1,21 @@
 import WhoAmI_File
 def test_WhoAmI():
     assert WhoAmI_File.WhoAmI != 'djr2132'
+
+test_WhoAmI()
         
 import BondPrice_File
 def test_getBondPrice():
-    assert round(BondPrice_File.getBondPrice(.03, 2000000, .04, 10,  1)) == 2170604
+    assert round(BondPrice_File.getBondPrice(.03, 2000000, .04, 10,  1)) == 217
     assert round(BondPrice_File.getBondPrice(.03, 2000000, .04, 10,  2)) == 2171686
+
+
+test_getBondPrice()
 
 import BondDuration_File
 def test_getBondDuration():
     assert round(BondDuration_File.getBondDuration(.03, 2000000, .04, 10,1),2) == 8.51
+test_getBondDuration()
 
 import BondPrice_E_File
 def test_GetBondPriceE():
@@ -17,7 +23,9 @@ def test_GetBondPriceE():
     face = 2000000
     couponRate = .04
     m = 10
-    assert round(BondPrice_E_File.getBondPrice_E(face, couponRate, m, yc)) == 2098949 
+    assert round(BondPrice_E_File.getBondPrice_E(face, couponRate, m, yc)) == 2098949
+
+test_GetBondPriceE()
 
 import BondPrice_Z_File
 def test_GetBondPriceZ():
@@ -27,10 +35,14 @@ def test_GetBondPriceZ():
     couponRate = .04
     x = BondPrice_Z_File.getBondPrice_Z(face, couponRate, times, yc)
     assert round(x) == 1996533
-    
+
+test_GetBondPriceZ()
+
 import FizzBuzz_File
 def test_FizzBuzz():
     x = FizzBuzz_File.FizzBuzz(40,45)
     assert x[0] == "buzz"
     assert x[1] == 41
     assert x[5] == "fizzbuzz"
+
+test_FizzBuzz()

d5921d18bbc5eeceeba7b5605890c7dc9a906477
awsmscan <maoqinyu226@gmail.com>
2020-11-27 02:44:18 -0500

    verison1.1

--- test_UnitTests.py
+++ test_UnitTests.py
@@ -6,7 +6,7 @@ test_WhoAmI()
         
 import BondPrice_File
 def test_getBondPrice():
-    assert round(BondPrice_File.getBondPrice(.03, 2000000, .04, 10,  1)) == 217
+    assert round(BondPrice_File.getBondPrice(.03, 2000000, .04, 10,  1)) == 2170604
     assert round(BondPrice_File.getBondPrice(.03, 2000000, .04, 10,  2)) == 2171686
 
 

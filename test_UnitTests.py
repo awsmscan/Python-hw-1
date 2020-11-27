6e001675d88cbf275982962c46e76889f142d3df
awsmscan <maoqinyu226@gmail.com>
2020-11-27 02:53:07 -0500

    verison 1.1

--- test_UnitTests.py
+++ test_UnitTests.py
@@ -2,7 +2,6 @@ import WhoAmI_File
 def test_WhoAmI():
     assert WhoAmI_File.WhoAmI != 'djr2132'
 
-test_WhoAmI()
         
 import BondPrice_File
 def test_getBondPrice():
@@ -10,12 +9,10 @@ def test_getBondPrice():
     assert round(BondPrice_File.getBondPrice(.03, 2000000, .04, 10,  2)) == 2171686
 
 
-test_getBondPrice()
 
 import BondDuration_File
 def test_getBondDuration():
     assert round(BondDuration_File.getBondDuration(.03, 2000000, .04, 10,1),2) == 8.51
-test_getBondDuration()
 
 import BondPrice_E_File
 def test_GetBondPriceE():
@@ -25,7 +22,6 @@ def test_GetBondPriceE():
     m = 10
     assert round(BondPrice_E_File.getBondPrice_E(face, couponRate, m, yc)) == 2098949
 
-test_GetBondPriceE()
 
 import BondPrice_Z_File
 def test_GetBondPriceZ():
@@ -36,7 +32,6 @@ def test_GetBondPriceZ():
     x = BondPrice_Z_File.getBondPrice_Z(face, couponRate, times, yc)
     assert round(x) == 1996533
 
-test_GetBondPriceZ()
 
 import FizzBuzz_File
 def test_FizzBuzz():
@@ -44,5 +39,3 @@ def test_FizzBuzz():
     assert x[0] == "buzz"
     assert x[1] == 41
     assert x[5] == "fizzbuzz"
-
-test_FizzBuzz()
